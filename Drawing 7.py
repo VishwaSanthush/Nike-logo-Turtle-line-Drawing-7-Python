06b7ff02b7e757d1441039da8f45042a58ef1876
Santhush Dharmawardhana <vishwasanthush666@gmail.com>
2022-07-25 00:25:24 +0530

    Add files via upload

---
+++ Drawing 7.py
@@ -0,0 +1,14 @@
+from turtle import *
+hideturtle()
+color('white')
+bgcolor('black')
+left(185)
+
+begin_fill()
+circle(30, 200)
+forward(200)
+left(173)
+forward(190)
+circle(-18, 170)
+end_fill()
+done()
